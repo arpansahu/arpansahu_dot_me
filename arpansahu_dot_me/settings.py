d98d73a74ef9125f148403a2a959241fb9d7f8b5
arpan sahu <arpanrocks95@gmail.com>
2022-11-30 01:59:47 +0530

    added MAIL_JET_EMAIL_ADDRESS and modified contact page

--- arpansahu_dot_me/settings.py
+++ arpansahu_dot_me/settings.py
@@ -16,7 +16,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MY_EMAIL_ADDRESS = config('MY_EMAIL_ADDRESS')
-
+MAIL_JET_EMAIL_ADDRESS = config('MAIL_JET_EMAIL_ADDRESS')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
