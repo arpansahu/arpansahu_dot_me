088e47f477e3a400483274361d05db74eeb8be0f
arpansahu <arpanrocks95@gmail.com>
2024-08-05 22:37:20 +0530

    updated shell for merge into build

--- merge_main_into_build_branch.sh
+++ merge_main_into_build_branch.sh
@@ -2,21 +2,19 @@
 
 # Function to set up the environment
 setup_environment() {
-    
-        # Path to the GIT_ASKPASS helper script
-        GIT_ASKPASS_HELPER="$(pwd)/git-askpass.sh"
+    # Path to the GIT_ASKPASS helper script
+    GIT_ASKPASS_HELPER="$(pwd)/git-askpass.sh"
 
-        # Create the GIT_ASKPASS helper script
-        echo "Creating GIT_ASKPASS helper script"
-        cat <<EOF > "$GIT_ASKPASS_HELPER"
+    # Create the GIT_ASKPASS helper script
+    echo "Creating GIT_ASKPASS helper script"
+    cat <<EOF > "$GIT_ASKPASS_HELPER"
 #!/bin/sh
 echo \$GIT_PASSWORD
 EOF
-        chmod +x "$GIT_ASKPASS_HELPER"
+    chmod +x "$GIT_ASKPASS_HELPER"
 
-        # Export GIT_ASKPASS
-        export GIT_ASKPASS="$GIT_ASKPASS_HELPER"
-
+    # Export GIT_ASKPASS
+    export GIT_ASKPASS="$GIT_ASKPASS_HELPER"
 }
 
 # Directory where the script is located
@@ -31,7 +29,6 @@ merge_main_into_build() {
     REPO_PATH="${repo_url#https://github.com/}"
 
     # Construct the authenticated URL for prod
-    
     AUTHENTICATED_URL="https://${GIT_USERNAME}@github.com/${REPO_PATH}"
    
     # Log the URL being used (without exposing the password)
@@ -43,19 +40,23 @@ merge_main_into_build() {
         echo "Successfully cloned repository: $repo_url"
     else
         echo "Failed to clone repository: $repo_url"
-        return
+        return 1
     fi
 
     # Navigate to the repository directory
-    cd "$repo_name" || { echo "Failed to navigate to repository directory: $repo_name"; return; }
+    cd "$repo_name" || { echo "Failed to navigate to repository directory: $repo_name"; return 1; }
     
-    # Check if readme.html was created or updated
-    git checkout build
-    git rebase origin/main
-    git push 
+    # Check if the build branch exists and merge main into build
+    if git rev-parse --verify build > /dev/null 2>&1; then
+        git checkout build
+        git rebase origin/main
+        git push
+    else
+        echo "Branch 'build' does not exist in repository: $repo_url"
+    fi
     
     # Navigate back to the script directory
-    cd "../.."
+    cd "$SCRIPT_DIR"
 
     # Remove the cloned repository
     echo "Cleaning up ..."
@@ -64,14 +65,12 @@ merge_main_into_build() {
 
 # Main script execution
 main() {
-
-    # This scrip is built for prod only
+    # This script is built for prod only
     # Setup the environment
     setup_environment
     mkdir "playground_dir"
     cd "playground_dir"
     merge_main_into_build "https://github.com/arpansahu/arpansahu_dot_me"
-
 }
 
 # Execute the main function with provided arguments or default to prod environment and all repositories
